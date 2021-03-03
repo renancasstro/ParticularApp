1454ddebb6fb79d31841b000be10c42421c60180
Renan Castro <renancruz.castro@gmail.com>
2021-03-03 17:14:49 -0300

    Teste Versionamento

--- SimuDriver.js
+++ SimuDriver.js
@@ -2,7 +2,7 @@
 
 distancia = parseInt(prompt('Quantos Km tem sua corrida?'))
 tempo = parseInt(prompt('Quanto tempo de duração, em minutos, tem sua corrida?'))
-pedagio = parseInt(prompt('Informe o valor somado dos pedágios?'))
+pedagio = parseInt(prompt('Informe o valor somado dos pedágios:'))
 estimativa = 1.50+distancia*1.20+tempo*0.1125+pedagio
 
 document.write('Você vai ganhar R$'+estimativa.toFixed(2)+'<br>')
